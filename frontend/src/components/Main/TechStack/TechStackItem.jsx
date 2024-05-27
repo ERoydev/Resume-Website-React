b765e9f203898cb9050470325ca180678d1083fd
Emil <e.roydev@gmail.com>
2024-05-27 18:33:53 +0300

    fixed image loading on my skills

--- frontend/src/components/Main/TechStack/TechStackItem.jsx
+++ frontend/src/components/Main/TechStack/TechStackItem.jsx
@@ -2,6 +2,7 @@ export default function TechStackItem({
     img,
     name
 }) {
+
     return(
         <div className="item upwards">
             <div>
---
+++ frontend/src/components/Main/TechStack/data.js
@@ -0,0 +1,31 @@
+import pythonImg from '../../../assets/styles/images/technologies/python.svg';
+import javascriptImg from '../../../assets/styles/images/technologies/javascript.svg';
+import htmlImg from '../../../assets/styles/images/technologies/html.svg';
+import cssImg from '../../../assets/styles/images/technologies/css.svg';
+import reactImg from '../../../assets/styles/images/technologies/react.svg';
+import djangoImg from '../../../assets/styles/images/technologies/django.svg';
+import postgreImg from '../../../assets/styles/images/technologies/postgresql.svg';
+import nodejsImg from '../../../assets/styles/images/technologies/nodejs.svg';
+import githubImg from '../../../assets/styles/images/technologies/github.svg';
+import linuxImg from '../../../assets/styles/images/technologies/linux.svg';
+import dockerImg from '../../../assets/styles/images/technologies/docker.svg';
+import photoshopImg from '../../../assets/styles/images/technologies/photoshop.svg';
+import figmaImg from '../../../assets/styles/images/technologies/figma.svg';
+
+const techStackData = [
+    {name: "Python", imageUrl: pythonImg},
+    {name: "JavaScript", imageUrl: javascriptImg},
+    {name: "HTML", imageUrl: htmlImg},
+    {name: "CSS", imageUrl: cssImg},
+    {name: "ReactJS", imageUrl: reactImg},
+    {name: "Django", imageUrl: djangoImg},
+    {name: "PostgreSQL", imageUrl: postgreImg},
+    {name: "NodeJS", imageUrl: nodejsImg},
+    {name: "Github", imageUrl: githubImg},
+    {name: "Linux", imageUrl: linuxImg},
+    {name: "Docker", imageUrl: dockerImg},
+    {name: "Photoshop", imageUrl: photoshopImg},
+    {name: "Figma", imageUrl: figmaImg},
+]
+
+export default techStackData;
